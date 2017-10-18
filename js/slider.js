a8b0dcdb9f475a482f7b63803d9b88e80672701f
MichaelFoox <kamyanskiy.i@gmail.com>
2017-10-18 16:18:25 +0300

    About us block

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,14 @@
-/**
- * Created by Vlad on 18.10.2017.
- */
+'use strict'
+
+new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+    // If we need pagination
+    pagination: '.swiper-pagination',
+
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+});
