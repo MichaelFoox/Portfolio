c483cfc065cc976719c065b1af5b6f3a1e5df78e
MichaelFoox <kamyanskiy.i@gmail.com>
2017-10-21 16:03:16 +0300

    Fixed navbar, benefit block

---
+++ js/header.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var h_hght = 0; // высота шапки
+var h_mrg = 0;    // отступ когда шапка уже не видна
+
+$(function(){
+
+    var elem = $('#gmenu');
+    var top = $(this).scrollTop();
+
+    if(top > h_hght){
+        elem.css('top', h_mrg);
+    }
+
+    $(window).scroll(function(){
+        top = $(this).scrollTop();
+
+        if (top+h_mrg < h_hght) {
+            elem.css('top', (h_hght - top));
+        } else {
+            elem.css('top', h_mrg);
+        }
+
+        if (top+h_mrg > h_hght) {
+            elem.css('top', (h_mrg - h_hght));
+        } else {
+            elem.css('top', h_hght);
+        }
+    });
+
+});
